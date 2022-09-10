98db58182cba74add99a70b0ec6daf3ed8d1e87c
teri <teri.fash@gmail.com>
2022-09-10 06:45:05 +0300

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
   root: true,
   env: {
-		browser: true,
+    browser: true,
     es6: true,
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     'prettier',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
-    "vue/multi-word-component-names": 0,
-		"@typescript-eslint/no-explicit-any": "off",
-		"@typescript-eslint/no-unused-vars": "off",
-		'@typescript-eslint/no-var-requires': 'off',
-		"no-undef": "off",
-		"vue/no-v-html": "off",
+    'vue/multi-word-component-names': 0,
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-undef': 'off',
+    'vue/no-v-html': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "tabWidth": 2,
-  "printWidth": 80,
-  "useTabs": false,
-  "semi": true,
-  "singleQuote": true,
-	"plugins": [require("prettier-plugin-tailwindcss")],
-  "tailwindConfig": "./tailwind.config.js"
-}
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.js',
+};
