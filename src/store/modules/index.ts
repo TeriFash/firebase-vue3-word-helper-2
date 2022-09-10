3411e8c60926805ea07c4b6634f092f51d430d28
teri <teri.fash@gmail.com>
2022-09-10 06:50:06 +0300

    add

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
-import { IAppState, app } from 'app.ts';
-import { IAppUser, user } from 'user.ts';
-import { IAppWords, words } from 'words.ts';
+import { IAppState, app } from './app.ts';
+import { IAppUser, user } from './user.ts';
+import { IAppWords, words } from './words.ts';
 
 export interface IStore {
   app: IAppState;
